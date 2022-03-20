f8bc86d4e3a77218bb140298f9415f4376a0bf25
Roman13456 <romankhromishin@gmail.com>
2022-03-20 17:33:07 +0200

    add not working carousel

---
+++ JS/Scripts.js
@@ -0,0 +1,21 @@
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
