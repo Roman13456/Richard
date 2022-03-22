8920ae89d9b2e41f94c7594a7f1b044672b2c826
Roman13456 <romankhromishin@gmail.com>
2022-03-22 13:56:36 +0200

    some adjustments

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -35,3 +35,13 @@ $(document).ready(function(){
         }
     });
 });
+
+    $("#nav").addClass("js").before('<div id="menu">&#9776;</div>');
+    $("#menu").click(function(){
+    $("#nav").toggle();
+});
+    $(window).resize(function(){
+    if(window.innerWidth > 768) {
+    $("#nav").removeAttr("style");
+}
+});
